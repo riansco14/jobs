f73786d9f7f741f48f47ddc42161d89e37ffdcff
riansco14 <rian.rabelo@ufrpe.br>
2021-11-04 22:15:11 -0300

    Tema do Styled COmponents

---
+++ mobile/pokemon/src/global/styles/theme.ts
@@ -0,0 +1,17 @@
+export default {
+    colors: {
+        primary: '#2E6EB5',
+        shape: '#ffffff',
+        black: '#000000',
+
+
+    },
+    fonts: {
+        spartan300: 'Spartan-Light',
+        spartan400: 'Spartan-Regular',
+        spartan500: 'Spartan-Medium',
+        spartan600: 'Spartan-SemiBold',
+        spartan700: 'Spartan-Bold',
+        spartan800: 'Spartan-ExtraBold',
+    },
+};
